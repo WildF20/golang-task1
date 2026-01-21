88701a04225317460454bbc5d990303811e76211
Wildan Febrian <wildanfebrian9@gmail.com>
2026-01-22 06:54:01 +0700

    create routes handler

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"golang-task1/routes"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	routes.RegisterRoutes(mux)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
