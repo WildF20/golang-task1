9b35e59e021e13bd8c97424f89c2dcd3598497af
Wildan Febrian <wildanfebrian9@gmail.com>
2026-01-22 08:58:36 +0700

    get all categories

--- handler/categories_handler.go
+++ handler/categories_handler.go
@@ -2,11 +2,21 @@ package handler
 
 import (
 	"net/http"
+	"encoding/json"
+
 	"golang-task1/model"
 )
 
+var categories = []model.Category{}
+
 func GetAllCategories(w http.ResponseWriter, r *http.Request) {
-	return
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(categories); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func GetCategoryByID(w http.ResponseWriter, r *http.Request) {
