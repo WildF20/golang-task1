693111e2536717ed4aa6841ac4cfc6bb50a36107
Wildan Febrian <wildanfebrian9@gmail.com>
2026-01-22 08:17:53 +0700

    create model and handler

---
+++ handler/categories_handler.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"net/http"
+	"golang-task1/model"
+)
+
+func GetAllCategories(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func GetCategoryByID(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func CreateCategory(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func UpdateCategory(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func DeleteCategory(w http.ResponseWriter, r *http.Request) {
+	return
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,8 +2,14 @@ package routes
 
 import (
 	"net/http"
+
+	"golang-task1/handler"
 )
 
 func RegisterRoutes(mux *http.ServeMux) {
-	
+	mux.HandleFunc("GET /categories", handler.GetAllCategories)
+	mux.HandleFunc("GET /categories/{id}", handler.GetCategoryByID)
+	mux.HandleFunc("POST /categories", handler.CreateCategory)
+	mux.HandleFunc("PUT /categories/{id}", handler.UpdateCategory)
+	mux.HandleFunc("DELETE /categories/{id}", handler.DeleteCategory)
 }
